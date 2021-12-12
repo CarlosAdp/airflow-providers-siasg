c800393eb59d5cbc1a3ab59605f6f71a46adcfd6
Carlos Alberto Duarte Pinto <carlos.adpinto@gmail.com>
2021-12-12 14:34:39 -0300

    Finaliza transfer para Mongo

--- airflow/providers/siasg/example_dags/dw.py
+++ airflow/providers/siasg/example_dags/dw.py
@@ -5,6 +5,8 @@ from airflow.decorators import dag
 
 from airflow.providers.siasg.dw.transfers.relatorio_para_arquivo \
     import DWSIASGRelatorioParaArquivoOperator
+from airflow.providers.siasg.dw.transfers.relatorio_para_mongo \
+    import DWSIASGRelatorioParaMongoOperator
 
 
 @dag(schedule_interval=None, start_date=datetime(2021, 12, 10))
@@ -17,7 +19,19 @@ def teste_siasg():
         respostas_prompts=['160030', '160130']
     )
 
+    task2 = DWSIASGRelatorioParaMongoOperator(
+        task_id='task2',
+        id_conexao='teste',
+        id_relatorio='BFD128CD11EC5B5D670B0080EF6553F5',
+        respostas_prompts=['160030', '160130'],
+        timeout_segundos=120,
+        id_conexao_mongo='teste_mongo',
+        banco='teste_siasg',
+        colecao='teste_siasg',
+    )
+
     task1
+    task2
 
 
 dag = teste_siasg()
